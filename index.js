4322b9852fe34d7476ad92f0c4f49b1a4da3fd61
Nawed <team-service@coyoapp.com>
2020-10-13 15:02:33 +0200

    Change Functions and Updated to Manifest 1.2.0

--- index.js
+++ index.js
@@ -4,18 +4,24 @@ const src = window.location.search.match(/[?&]src=([^&]*)/)[1]
 
 parent.postMessage(
 {
-    source: src,
-    topic: 'requestData',
-    payload: [
-    'userName',
+    iss: src,
+    sub: 'requestData',
+    data: [
+        "tenantId",
+        "tenantUrl",
+        "userId",
+        "userName",
+        "userEmail",
+        "userLanguage"
     ]
 }, "*"
 )
 window.addEventListener('message', function receiveData(event) {
-var message = event.data;
-if (message.source === 'coyo:app' && message.topic === 'provideData') {
-    var jwt = message.payload;
-    var userData = jwt_decode(jwt);
-    document.getElementById("userName").textContent = userData.userName;
-}
+    var jwt = jwt_decode(event.data);
+    document.getElementById("tenantId").textContent = jwt["data.tenantId"];
+    document.getElementById("tenantUrl").textContent = jwt["data.tenantUrl"];
+    document.getElementById("userId").textContent = jwt["data.userId"];
+    document.getElementById("userName").textContent = jwt["data.userName"];
+    document.getElementById("userEmail").textContent = jwt["data.userEmail"];
+    document.getElementById("userLanguage").textContent = jwt["data.userLanguage"];
 });
